a8c9057accad1101a9d08152fa52c31bf47b0e1b
MacBookAir <macbookair@MacBookAirs-MacBook-Air.local>
2025-08-19 18:17:37 -0400

    chore: Finalize configuration for deployment

--- lib/withAuth.ts
+++ lib/withAuth.ts
@@ -1,11 +1,14 @@
 import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
-import jwt from 'jsonwebtoken';
+import { jwtVerify } from 'jose';
 
-const verifyToken = (req: NextApiRequest) => {
+const verifyToken = async (req: NextApiRequest) => {
   const token = req.cookies.auth_token;
   if (!token) return null;
+
   try {
-    return jwt.verify(token, process.env.JWT_SECRET as string);
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET as string);
+    const { payload } = await jwtVerify(token, secret);
+    return payload;
   } catch (e) {
     return null;
   }
@@ -13,7 +16,7 @@ const verifyToken = (req: NextApiRequest) => {
 
 export const withAuth = (handler: NextApiHandler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
-    if (!verifyToken(req)) {
+    if (!(await verifyToken(req))) {
       return res.status(401).json({ success: false, message: 'Authentication required.' });
     }
     return handler(req, res);
